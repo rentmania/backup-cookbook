9de64ff060d6339048f57d9f28bdaa365f34097b
Vitaly Perminov <perminovx@gmail.com>
2017-07-24 20:03:52 +0300

    misc

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'perminovx@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures rentmania-backup'
 long_description 'Installs/Configures rentmania-backup'
-version          '0.1.6'
+version          '0.2.0'
+
+depends 'backup'
