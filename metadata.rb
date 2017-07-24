5140e88aa09a86c7a84b00e9053eed0c84a1f38e
Vitaly Perminov <perminovx@gmail.com>
2017-07-24 15:06:00 +0300

    initial

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'backup-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures backup-cookbook'
+long_description 'Installs/Configures backup-cookbook'
+version          '0.1.0'
